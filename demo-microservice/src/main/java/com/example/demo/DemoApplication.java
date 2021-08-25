4973bac40c2a4b93aaa40e5164d937aa94ca90b4
MehrabRahman <mehrab.rahman@revature.com>
2021-08-25 10:25:12 -0500

    Adding gateway & discovery services

--- demo-microservice/src/main/java/com/example/demo/DemoApplication.java
+++ demo-microservice/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
+@EnableEurekaClient
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public Mono<String> hello() {
+		return Mono.just("Hello world");
 	}
 
 }
